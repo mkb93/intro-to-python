1aaf5e7e32a69090086875698db45e3f69ef268b
Marco Karim belahouane <nfk18@hotmail.co.uk>
2024-04-01 15:14:24 +0100

    done some basic exercises

--- 027_comparison.py
+++ 027_comparison.py
@@ -39,8 +39,7 @@ print("Function: a_is_less_than_b")
 
 def a_is_less_than_b(a, b):
   # Uncomment this next line and replace ?? with the right operator
-  # return a ?? b
-  pass
+  return a < b
 
 check_that_these_are_equal(
   a_is_less_than_b(1, 2),
@@ -63,8 +62,7 @@ print("")
 print("Function: a_is_greater_than_b")
 
 def a_is_greater_than_b(a, b):
-  # return a ?? b
-  pass
+  return a > b
 
 check_that_these_are_equal(
   a_is_greater_than_b(1, 2),
@@ -87,8 +85,7 @@ print("")
 print("Function: a_is_less_than_or_equal_to_b")
 
 def a_is_less_than_or_equal_to_b(a, b):
-  # return a ?? b
-  pass
+  return a <= b
 
 check_that_these_are_equal(
   a_is_less_than_or_equal_to_b(1, 2),
@@ -111,7 +108,7 @@ print("")
 print("Function: a_is_greater_than_or_equal_to_b")
 
 def a_is_greater_than_or_equal_to_b(a, b):
-  # return a ?? b
+  return a >= b
   pass
 
 check_that_these_are_equal(
@@ -135,7 +132,7 @@ print("")
 print("Function: a_is_not_equal_to_b")
 
 def a_is_not_equal_to_b(a, b):
-  # return a ?? b
+  return a != b
   pass
 
 check_that_these_are_equal(
@@ -161,7 +158,7 @@ print("Function: a_is_within_b")
 # May be a little tricky — search for
 # "python check if string contains substring"
 def a_is_within_b(a, b):
-  # return a ?? b
+  return a in b
   pass
 
 check_that_these_are_equal(
